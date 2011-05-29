8e162747d2c9f407c3b91939c55dbfa32baae6fa
victor <victor@pictorical.com>
2011-05-28 20:09:27 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tapir::Application.config.secret_token = '6964d63afed8519ce85d0786613f0e9d4509bb2a3eff28772743872a22c80c59981bf368cba06ddd32b536554c8bd0013fad248941000a2c719ccc5d3a1fc2b6'
