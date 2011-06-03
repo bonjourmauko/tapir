71b3baacc77fd2bdf7644c8c141e246264b157a7
victor <victor@pictorical.com>
2011-06-02 22:47:57 -0400

    typo

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -27,7 +27,7 @@ class SourcesController < ApplicationController
     source.save!
     book.save!
 
-    Notifier.welcome(book).deliver
+    Transactional.welcome(book).deliver
 
     render :text => "Created succesfully!\n", :status => 200
     
